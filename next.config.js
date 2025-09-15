9d2393bf647da1e18b4b996bf2a7843e6ce8ab70
Eiran <iradukundaeiran@gmail.com>
2025-09-15 02:47:28 -0700

    separate frontend from backend

--- next.config.js
+++ next.config.js
@@ -6,28 +6,28 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8002/api/:path*"
+            ? "https://space.abn.rw/api/:path*"
             : "/api/",
       },
       {
         source: "/static/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8002/static/:path*"
+            ? "https://space.abn.rw/static/:path*"
             : "/static/:path*",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8002/docs"
+            ? "https://space.abn.rw/docs"
             : "/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8002/openapi.json"
+            ? "https://space.abn.rw/openapi.json"
             : "/api/openapi.json",
       },
     ];
